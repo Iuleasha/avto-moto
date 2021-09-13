fadc71a6894f6b86630a27050217c66ece032fda
Iulia <iuleasha7@mail.ru>
2021-09-13 23:39:56 +0300

    правки по чек-листу

--- src/components/feedback-dialog/feedbackDialog.js
+++ src/components/feedback-dialog/feedbackDialog.js
@@ -1,10 +1,31 @@
 import './feedbackDialog.scss';
 import FeedbackForm from '../feedback-form/feedback-form';
 import { FeedbackDialogType } from '../../types/types';
+import { useCallback, useEffect } from 'react';
 
 function FeedbackDialog({ onCloseEvent }) {
+    const onCloseDialogEvent = useCallback(
+        (evt) => {
+            if (
+                evt.keyCode === 27 ||
+                evt.target.className.includes('dialog__overlay')
+            ) {
+                onCloseEvent();
+            }
+        },
+        [onCloseEvent]
+    );
+
+    useEffect(() => {
+        document.addEventListener('keydown', onCloseDialogEvent, false);
+
+        return () => {
+            document.removeEventListener('keydown', onCloseDialogEvent, false);
+        };
+    }, [onCloseDialogEvent]);
+
     return (
-        <div className="dialog__overlay">
+        <div className="dialog__overlay" onClick={onCloseDialogEvent}>
             <div className="dialog__content">
                 <button
                     className="dialog__close-button"
